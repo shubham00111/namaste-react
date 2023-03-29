29f982a48c2e53a33aded6521c793b026ae3ac6b
unknown <shubhamrawat858@yahoo.in>
2023-03-30 02:22:19 +0530

    Added react and react-dom

--- App.js
+++ App.js
@@ -1,17 +1,5 @@
-/*
-<div id='parent'>
-    <div id='child1'>
-        <h1>Hello there 1</h1>
-        <h2>Hi there</h2>
-    </div>
-    
-    <div id='child2'>
-        <h1>Hello there 1</h1>
-        <h2>Hi there</h2>
-    </div>
-
-</div>
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
